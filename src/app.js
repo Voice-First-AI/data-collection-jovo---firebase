391432c5029e755e598b0eac564866ccb8b57a98
Sweetmantech <sweetmantech@gmail.com>
2020-07-17 09:46:27 -0400

    I complete initial functioning middleware hook

--- src/app.js
+++ src/app.js
@@ -5,6 +5,15 @@ const { Alexa } = require('jovo-platform-alexa');
 const { GoogleAssistant } = require('jovo-platform-googleassistant');
 const { JovoDebugger } = require('jovo-plugin-debugger');
 const { FileDb } = require('jovo-db-filedb');
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
 
 // ------------------------------------------------------------------
 // APP INITIALIZATION
@@ -35,6 +44,33 @@ app.setHandler({
   MyNameIsIntent() {
     this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
   },
+});
+
+app.hook('before.platform.output', async (error, host, jovo) => {
+  const request = jovo.$request;
+  const session = jovo.$request['session']
+  const manualRequestObject = request['request'];
+  await db.collection("users").doc("user-id").collection("REQUEST_DATA").doc(request.getTimestamp()).set({
+      timestamp: request.getTimestamp(),
+      userID: request.getUserId(),
+      intent: request.getIntentName() ? request.getIntentName() : manualRequestObject['type'],
+      inputs: request.getInputs(),
+      isNewSession: request.isNewSession(),
+      sessionId: session['sessionId'],
+      deviceName: request.getDeviceName(),
+      locale: request.getLocale(),
+      deviceHasAudioInterface: request.hasAudioInterface(),
+      deviceHasScreenInterface: request.hasScreenInterface(),
+      deviceHasVideoInterface: request.hasVideoInterface(),
+      sessionAttributes: request.getSessionData() ? request.getSessionData() : "no session attributes",
+      deviceID: request['context']["System"]['device']['deviceId'] ? request['context']["System"]['device']['deviceId'] : "no device ID found"
+  })
+  .then(function() {
+      console.log("Request data successfully written to Firestore!");
+  })
+  .catch(function(error) {
+      console.error("Error writing document: ", error);
+  });
 });
 
 module.exports = { app };
